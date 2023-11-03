df1e02c717a08b09b0c12e0c97455c0f66be7416
Learner Getready <learnergit@hymyilee.fi>
2023-11-03 18:16:35 +0200

    2.2

--- pbbackend/index.js
+++ pbbackend/index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const dateTime = new Date()
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${dateTime}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
